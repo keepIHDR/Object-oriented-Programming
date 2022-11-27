7f009295ee451d628ac328e00bbe9abbba3a3a78
Zehao Zhang <ze_hao2002@163.com>
2022-11-27 23:15:02 +0800

    Experiment-4_1

---
+++ OOP_Experiment/Experiment-4_1/Experiment-4_1_2/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+#include"point.h"
+
+double distance(Point& A, Point& B) {
+	double ans = (A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y);
+	return sqrt(ans);
+}
+
+int main() {
+	Point M(2, 3), N(5, 7);
+	cout << "点 M 和点 N 之间的距离为: " << distance(M, N) << endl;
+	system("pause");
+	return 0;
+}
